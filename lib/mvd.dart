21d23b9807c3b424eb4adc48205db54de97761d4
sidharth-pb <83686876+sidharth-pb@users.noreply.github.com>
2021-09-30 14:11:36 +0530

    Update mvd.dart

--- lib/mvd.dart
+++ lib/mvd.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:hackerhike/jsonpage.dart';
 
 class Mvd extends StatelessWidget{
   @override
@@ -35,39 +36,28 @@ class Mvd extends StatelessWidget{
                     children: [
                       SizedBox(height:35),
                       Divider(thickness:2,color: Colors.black,),
-                      ExpansionTile(
-                        leading:Icon(Icons.add_chart),
-                        iconColor: Colors.black,
-                        textColor: Colors.green,
-                        title: Text('Production of documents'),
-                        children: <Widget>[
-                          ListTile(leading:Icon(Icons.circle,color:Colors.black),title: Text('Driving Licence')),
-                          ListTile(leading:Icon(Icons.circle,color: Colors.black,),title: Text('Regestration Certificate')),
-                        ],
-                      ),
-                      Divider(thickness:2,color: Colors.black,),
-                      ExpansionTile(
-                        leading:Icon(Icons.add_chart),
-                        iconColor: Colors.black,
-                        textColor: Colors.green,
-                        title: Text('Towig'),
-                        children: <Widget>[
-                          ListTile(leading:Icon(Icons.circle,color:Colors.black),title: Text('No vehicle other than a mechanically disabled motor vehicle or incompletely assembled motor vehicle, a registered trailer or a side car, shall be drawn or towed by any motor vehicle, except for purposes of delivery and to the nearest filling station or garage.')),
-                          ListTile(leading:Icon(Icons.circle,color: Colors.black,),title: Text('No motor vehicle when towing anothervehicles other than a trailer or sidecar shall be driven at a speed exceeding twenty-four kilometres per hour.')),
-                        ],
-                      ),
-                      Divider(thickness:2,color: Colors.black,),
-                      ExpansionTile(
-                        leading:Icon(Icons.add_chart),
-                        iconColor: Colors.black,
-                        textColor: Colors.green,
-                        title: Text('Driving of Tractors and ood vehicles'),
-                        children: <Widget>[
-                          ListTile(leading:Icon(Icons.circle,color:Colors.black),title: Text('A driver when driving a tractor shall not carry or allow any person to be carried on tractor. A driver of goods carriage shall not carry in the driver''s cabin more number of persons than that is mentioned in the registration certificate and shall not carry passengers for hire or reward.')),
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                            return JsonPage();
+                          }));
+                        },
+                        child: Container(
+                          color:Colors.white,
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          child:Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children:[
+                                Icon(Icons.ac_unit),
+                                Text('Production of Document'),
+                                Icon(Icons.arrow_forward)
+                              ])
 
-                        ],
+                        ),
                       ),
                       Divider(thickness:2,color: Colors.black,),
+                      
                     ],
                   ),
                 ),
